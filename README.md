824cd4cc9c1a3fb378b31868bb7c8670e890b517
Mark Katerberg <katerberg@fastmail.fm>
2018-09-17 22:37:17 -0500

    Baseline server

---
+++ README.md
@@ -0,0 +1,18 @@
+# Minimal Project Start
+
+[![Build Status](https://travis-ci.com/katerberg/minimal-node.svg?branch=master)](https://travis-ci.com/katerberg/minimal-node)
+
+## Fast Start
+
+```
+npm install
+npm start
+```
+
+
+## Setup
+
+This project was set up with some tutorials:
+
+ - https://github.com/jsynowiec/node-typescript-boilerplate
+ - https://docs.travis-ci.com/user/getting-started/
---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import * as bodyParser from "body-parser";
+import * as compression from "compression";  // compresses requests
+import * as express from "express";
+import * as expressValidator from "express-validator";
+import * as path from "path";
+
+
+// Controllers (route handlers)
+import * as homeController from "./controllers/home";
+
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+
+app.use(
+  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+/**
+ * Primary app routes.
+ */
+app.get("/", homeController.index);
+
+export default app;
