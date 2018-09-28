ceab818f200bee35d749d7576e53c081ac861ee6
Jose Berrio <aldarisbm@gmail.com>
2018-09-28 10:30:21 -0500

    changed from [object] to object[]

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -5,8 +5,8 @@ import { users } from '../models/users';
 const db = mongoose.connection;
 
 export let employees = (req: Request, res: Response) => {
-	const query = users.find({}, (err: Error, data: [object]) => {
-		const userData: [object] = data;
+	const query = users.find({}, (err: Error, data: object[]) => {
+		const userData: object[] = data;
 		res.status(200).send(userData);
 	});
 };
--- src/models/users.ts
+++ src/models/users.ts
@@ -1,14 +1,10 @@
-import { mongoose } from '../config/database';
+import * as mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
 const users = mongoose.model(
 	'users',
-	new Schema({
-		email: String,
-		name: String,
-		phone: String,
-	})
+	new Schema({ email: String, name: String, phone: String })
 );
 
 export { users };
