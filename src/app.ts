e5f15027cec0d1bb4080a8dc02a95f3fab5bcda5
Emily Kraft <ek1074@gmail.com>
2018-10-02 12:57:13 -0500

    committing after pull

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,12 @@ app.use(
 	express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 })
 );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /**
  * Primary app routes.
  */
